e5807e189ba1bec48f75011b93c7a7e7f96e85ee
JaydenSoong <jiandongsong@126.com>
2017-11-24 17:12:39 +0800

    得到第一个 name 标签的值

--- day06/src/com/jayden/dom4jtest/Foo.java
+++ day06/src/com/jayden/dom4jtest/Foo.java
@@ -9,17 +9,20 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 class Foo {
+    private Document getDocument() throws DocumentException {
+        // 得到 SAXReader
+        SAXReader reader = new SAXReader();
+        // 返回 Document
+        return reader.read("xml/user.xml");
+    }
 
     /**
      * 得到所有 name 标签的值
      */
    @Test
    void getNodeText() throws DocumentException {
-       // 得到 SAXReader
-       SAXReader reader = new SAXReader();
        // 得到 Document
-       Document document = reader.read("xml/user.xml");
-
+       Document document = getDocument();
        // 得到 Document 的根元素
        Element root = document.getRootElement();
        // 得到根元素的所有第一层名为 user 的子元素
@@ -31,4 +34,21 @@ class Foo {
            System.out.println(element1.getText());
        }
    }
+
+    /**
+     * 得到第一个 name 标签的值
+     */
+    @Test
+    void getFirstNodeText() throws DocumentException {
+        // 得到 Document
+        Document document = getDocument();
+        // 得到根元素
+        Element root = document.getRootElement();
+        // 得到第一个 user
+        Element firstUser = root.element("user");
+        // 得到第一个 name
+        Element firstName = firstUser.element("name");
+        // 输出 name 的值
+        System.out.println(firstName.getText());
+    }
 }
