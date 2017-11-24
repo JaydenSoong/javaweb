89c03f884b10f64cdf7c21b34f361e06a4985461
JaydenSoong <jiandongsong@126.com>
2017-11-24 21:46:28 +0800

    在指定位置添加节点

--- day06/src/com/jayden/dom4jtest/Foo.java
+++ day06/src/com/jayden/dom4jtest/Foo.java
@@ -2,6 +2,7 @@ package com.jayden.dom4jtest;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 import org.dom4j.io.OutputFormat;
 import org.dom4j.io.SAXReader;
@@ -20,6 +21,16 @@ class Foo {
         return reader.read("xml/user.xml");
     }
 
+    /**
+     * 回写方法
+     */
+    private void xmlWriter(Document document) throws IOException {
+        // 回写，第一个参数：要回写到哪里，第二个参数：带格式（给缩进）
+        XMLWriter writer = new XMLWriter(new FileOutputStream("xml/user.xml"), OutputFormat.createPrettyPrint());
+        writer.write(document);
+        // 关流
+        writer.close();
+    }
     /**
      * 得到所有 name 标签的值
      */
@@ -90,11 +101,28 @@ class Foo {
         Element sex = user1.addElement("sex");
         // 往节点中添加文本节点
         sex.addText("male");
+        // 回写
+        xmlWriter(document);
+    }
 
-        // 回写，第一个参数：要回写到哪里，第二个参数：带格式（给缩进）
-        XMLWriter writer = new XMLWriter(new FileOutputStream("xml/user.xml"), OutputFormat.createPrettyPrint());
-        writer.write(document);
-        // 关流
-        writer.close();
+
+
+    /**
+     * 在特定位置添加元素，比如在 password 后面添加 email
+     */
+    @Test
+    void addEmail() throws DocumentException, IOException {
+        Document document = getDocument();
+        Element root = document.getRootElement();
+        Element user1 = root.element("user");
+        // 得到 第一个 user 下面的所有元素
+        List<Element> elements = user1.elements();
+        // 创建节点
+        Element email = DocumentHelper.createElement("email");
+        email.addText("foo@126.com");
+        // 通过 List<E> 的 add(index, element) 方法往特定位置添加节点
+        elements.add(2, email);
+        // 回写
+        xmlWriter(document);
     }
 }
